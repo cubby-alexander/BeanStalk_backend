40bb6a6aa75e9205dd561c45c3f75fdae58447f5
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-07-20 16:00:04 -0400

    Added correct heroku link to cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rack::Cors do
   # end
 
   allow do
-    origin "https://beanstalk_api.heroku.com"
+    origin "https://beanstalk-api.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
